dbc4b11820cecea11a32e4606f5368e99ae5a460
Brian Cantoni <brian@cantoni.org>
2019-11-18 13:40:15 -0800

    Experiment with getting weather by zipcode instead of lat/lon

--- wxtext.py
+++ wxtext.py
@@ -23,17 +23,28 @@ def geocode(location, verbose=False):
     dat = json.loads(req.content)
     view = dat['Response']['View'][0]
 
+    if view['Result'][0]['Location']['Address'] == 'USA':
+        postalcode = view['Result'][0]['Location']['Address']['PostalCode']
+    else:
+        postalcode = False
+
+    if verbose:
+        print("Full found geolocation: {}\nPostal code: {}".format(
+            view['Result'][0]['Location']['Address']['Label'],
+            postalcode
+        ))
+
     return (view['Result'][0]['Location']['Address']['Label'],
             view['Result'][0]['Location']['DisplayPosition']['Latitude'],
-            view['Result'][0]['Location']['DisplayPosition']['Longitude'])
+            view['Result'][0]['Location']['DisplayPosition']['Longitude'],
+            postalcode)
 
 
-def weather(lat, lon, verbose=False):
-    """From lat/lon coordinates, find current weather report"""
-    url = "https://api.openweathermap.org/data/2.5/weather?APPID={}&lat={}&lon={}&units=imperial".format(
+def weather_by_zipcode(zipcode, verbose=False):
+    """From zipcode, find current weather report"""
+    url = "https://api.openweathermap.org/data/2.5/weather?APPID={}&zip={}&units=imperial".format(
         os.environ['OPENWEATHER_APP_ID'],
-        lat,
-        lon
+        zipcode
     )
     req = requests.get(url)
     if verbose:
@@ -51,10 +62,40 @@ def weather(lat, lon, verbose=False):
     return txt
 
 
+def weather(lat, lon, verbose=False):
+    """From lat/lon coordinates, find current weather report"""
+    url = "https://api.openweathermap.org/data/2.5/weather?APPID={}&lat={}&lon={}&units=imperial".format(
+        os.environ['OPENWEATHER_APP_ID'],
+        lat,
+        lon
+    )
+    req = requests.get(url)
+    if verbose:
+        print("WEATHER: {}".format(req.content))
+    dat = json.loads(req.content)
+    if 'deg' in dat['wind']:
+        degrees = compass.degrees_direction(dat['wind']['deg'])
+    else:
+        degrees = 0
+    txt = "{}: {}F, {}, humidity {}%, wind {}mph from {}".format(
+        dat['name'],
+        int(round(dat['main']['temp'])),
+        dat['weather'][0]['description'],
+        int(round(dat['main']['humidity'])),
+        int(round(dat['wind']['speed'])),
+        degrees
+    )
+
+    return txt
+
+
 def wxtext(location, verbose=False):
     """From text location description, find and summarize current weather report"""
-    (addr, lat, lon) = geocode(location, verbose)
-    wx = weather(lat, lon, verbose)
+    (addr, lat, lon, postalcode) = geocode(location, verbose)
+    if postalcode:
+        wx = weather_by_zipcode(postalcode, verbose)
+    else:
+        wx = weather(lat, lon, verbose)
     return wx
 
 
@@ -66,6 +107,7 @@ if __name__ == '__main__':
     tests = [
         'Soda Springs, CA',
         '95129',
+        '95014',
         'Joshua Tree',
         'Dublin Ireland'
     ]
