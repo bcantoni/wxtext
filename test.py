2f912643da3f5add3c184bb8e9c6ebbda1a76e28
Brian Cantoni <brian@cantoni.org>
2020-06-16 09:19:47 -0700

    Update test script: - convert from Python 2.7 to 3.7 - remove hard coded API keys (oops!) - add DarkSky weather here to match main script

--- test.py
+++ test.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 
 import json
+import os
 import requests
 import time
-import urllib
+import urllib.parse
 import compass
 
 
 def geocode(location):
     url = "https://geocoder.api.here.com/6.2/geocode.json?app_id={}&app_code={}&searchtext={}&countryfocus={}".format(
-        '3aOY6Lk040Jq7MYdHBRj',
-        'a1p7--0yzjDzUVZlxo1g-w',
-        urllib.quote(location),
+        os.environ['HERE_APP_ID'],
+        os.environ['HERE_APP_CODE'],
+        urllib.parse.quote(location),
         'USA'
     )
 
     req = requests.get(url)
+    assert req.status_code == 200, "Error calling geocoder service"
     dat = json.loads(req.content)
     view = dat['Response']['View'][0]
     print("geocode returned {} results".format(len(view['Result'])))
@@ -25,20 +27,51 @@ def geocode(location):
             view['Result'][0]['Location']['DisplayPosition']['Longitude'])
 
 
-def weather(lat, lon):
+def weather_open(lat, lon):
     url = "https://api.openweathermap.org/data/2.5/weather?APPID={}&lat={}&lon={}&units=imperial".format(
-        '2d4bf9e3c4df12236bcc3516175eabdc',
+        os.environ['OPENWEATHER_APP_ID'],
         lat,
         lon
     )
     req = requests.get(url)
+    assert req.status_code == 200, "Error calling weather service"
     dat = json.loads(req.content)
     txt = "{}: {} F, humidity {}%, wind {} from {}".format(
         dat['name'],
         dat['main']['temp'],
         dat['main']['humidity'],
         dat['wind']['speed'],
-        compass.wind_direction(dat['wind']['deg'])
+        compass.degrees_direction(dat['wind']['deg'])
+    )
+
+    return txt
+
+
+def weather_darksky(lat, lon, verbose=False):
+    """From lat/lon coordinates, find current weather report"""
+    url = "https://api.darksky.net/forecast/{}/{},{}?exclude=minutely,daily,alerts,hourly".format(
+        os.environ['DARKSKY_SECRET'],
+        lat,
+        lon
+    )
+    req = requests.get(url)
+    assert req.status_code == 200, "Error calling weather service"
+    if verbose:
+        print("WEATHER: {}".format(req.content))
+    dat = json.loads(req.content)
+
+    summary = dat['currently']['summary']
+    temp = int(round(dat['currently']['temperature']))
+    humidity = dat['currently']['humidity'] * 100
+    wind = int(round(dat['currently']['windSpeed']))
+    direction = compass.degrees_direction(dat['currently']['windBearing'])
+
+    txt = "{}F, {}, humidity {}%, wind {}mph from {}".format(
+        temp,
+        summary,
+        humidity,
+        wind,
+        direction
     )
 
     return txt
@@ -54,5 +87,5 @@ for p in places:
     time.sleep(1)
     (addr, lat, lon) = geocode(p)
     print("{} is at {} / {}".format(addr, lat, lon))
-    wx = weather(lat, lon)
+    wx = weather_darksky(lat, lon)
     print("Forecast: {}".format(wx))
